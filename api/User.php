db2c7235255d5e17130c69237cb0a47cbbc00d28
Juan Diego Fajardo <jfajardo@mit.edu>
2016-07-24 16:59:48 +0200

    Implement skeleton of User class

--- api/User.php
+++ api/User.php
@@ -11,5 +11,49 @@ namespace Models;
 
 class User
 {
+    /**
+     * @var string Username
+     */
+    public $user_name;
+
+    /**
+     * @var int User ID
+     */
+    public $user_id;
+
+    /**
+     * @var array Array of user privileges
+     */
+    public $user_privileges;
+
+    /**
+     * @var string User Email
+     */
+    public $user_email;
+
+
+    public function __construct() {
+
+    }
+
+    /**
+     * Method to verify token
+     *
+     * @param $token User token to verify
+     * @return bool token is valid
+     */
+    public function verify_token($token) {
+        
+    }
+
+    /**
+     * Method to return email from given token
+     *
+     * @param $token User token to verify
+     * @return string Email from given token
+     */
+    public function email_from_token($token) {
+
+    }
 
 }
