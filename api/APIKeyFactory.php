4be727a24099d551e4c20db77e82e516afc2b306
Juan Diego Fajardo <jfajardo@mit.edu>
2016-07-24 16:35:03 +0200

    Create new files for model classes

---
+++ api/APIKeyFactory.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: juanfajardo
+ * Date: 7/24/16
+ * Time: 4:26 PM
+ */
+
+namespace Models;
+
+
+final class APIKeyFactory
+{
+
+}
---
+++ api/User.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: juanfajardo
+ * Date: 7/24/16
+ * Time: 4:27 PM
+ */
+
+namespace Models;
+
+
+class User
+{
+
+}
