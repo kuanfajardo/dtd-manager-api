54f3e27b930114f76699a36d1aa8eb3c6f717a30
Juan Diego Fajardo <jfajardo@mit.edu>
2016-08-09 13:48:16 -0400

    Add implementation of api key generator and checking (only for iOS)

--- api/APIKeyFactory.php
+++ api/APIKeyFactory.php
@@ -59,7 +59,11 @@ final class APIKeyFactory
      * @return bool Key is valid
      */
     public static function verify_key($key, $origin) {
-        return true;
+        if ($key == '3Ha63GR28fbkknu29HUb1Qk3RO2NR9ga') {
+            return true;
+        }
+
+        return false;
     }
 
     /**
@@ -69,6 +73,29 @@ final class APIKeyFactory
      * @return string generated api key
      */
     public function generate_key($origin) {
-        return '';
+        return '3Ha63GR28fbkknu29HUb1Qk3RO2NR9ga';
+        //return $this->random_str(25);
+    }
+
+    /**
+     * Generate a random string, using a cryptographically secure
+     * pseudorandom number generator (random_int)
+     *
+     * For PHP 7, random_int is a PHP core function
+     * For PHP 5.x, depends on https://github.com/paragonie/random_compat
+     *
+     * @param int $length      How many characters do we want?
+     * @param string $keyspace A string of all possible characters
+     *                         to select from
+     * @return string
+     */
+    function random_str($length, $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    {
+        $str = '';
+        $max = mb_strlen($keyspace, '8bit') - 1;
+        for ($i = 0; $i < $length; ++$i) {
+            $str .= $keyspace[random_int(0, $max)];
+        }
+        return $str;
     }
 }
