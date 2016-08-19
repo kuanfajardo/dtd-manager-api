ff131a13e84ceb6defdbd637730f0299d07b461f
Juan Diego Fajardo <jfajardo@mit.edu>
2016-08-19 12:13:33 -0400

    Add syntax to fix error from previous commit

--- api/DeltsManagerAPI.php
+++ api/DeltsManagerAPI.php
@@ -169,7 +169,7 @@ class DeltsManagerAPI extends APIFramework
         return $punts;
         */
 
-        $punts_query = "SELECT id, IF(p.given_by > 0, (SELECT first FROM users WHERE id = p.given_by), 'Delts Manager'), comment, timestamp, makeup_timestamp, makeup_given_by, makeup_comment FROM punts WHERE user = {$this->User->user_id} ORDER BY timestamp DESC";
+        $punts_query = "SELECT id, IF(p.given_by > 0, (SELECT first FROM users WHERE id = p.given_by), 'Delts Manager'), comment, timestamp, makeup_timestamp, makeup_given_by, makeup_comment FROM punts p WHERE user = {$this->User->user_id} ORDER BY timestamp DESC";
         $punts = $mysqli->query($punts_query)->fetch_all(MYSQLI_ASSOC);
 
         return $punts;
