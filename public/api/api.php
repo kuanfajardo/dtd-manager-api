ee9e9740c83fc46867f2a362fb3bd02c7d2e44e9
Juan Diego Fajardo <jfajardo@mit.edu>
2016-12-13 16:16:33 -0500

    Move api files to main public file; edit paths in api.php

--- public/api/api.php
+++ public/api/api.php
@@ -7,9 +7,9 @@
  */
 
 // TODO: make sure this is correct path
-require_once ('../public/includes/db.php');
-require_once ('../public/includes/functions.php');
-require_once ('../public/includes/constants.php');
+require_once ('../includes/db.php');
+require_once ('../includes/functions.php');
+require_once ('../includes/constants.php');
 
 // Requests from the same server don't have a HTTP_ORIGIN header
 if(!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
